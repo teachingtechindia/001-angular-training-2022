67de4e4f47a6ba1d180645de26d733ee1ebbd4f9
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-05-19 16:45:49 +0530

    ngClass and ngStyle basic

--- src/app/components/layout/main-content/main-content.component.ts
+++ src/app/components/layout/main-content/main-content.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.scss'],
 })
 export class MainContentComponent implements OnInit {
-  script = '<h2>Some text beofere script elem</h2><script>alert("This alert will not be called!!")</script>';
+  script =
+    '<h2>Some text beofere script elem</h2><script>alert("This alert will not be called!!")</script>';
   div = '<h1>this is a div</h1>';
+
+  noOfProd = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  getDivStyle() {
+    if (this.noOfProd > 0) {
+      return { color: 'blue', backgroundColor: 'wheat' };
+    } else {
+      return { color: 'red' };
+    }
+  }
 }
