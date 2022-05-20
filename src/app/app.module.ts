e1f51f8ede9b653f874e8d97e40594ae3ed333da
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-05-20 17:51:16 +0530

    Angular router basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { HeaderComponent } from './components/layout/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { MainContentComponent } from './components/layout/main-content/main-content.component';
-import { ParentComponent } from './components/test/parent/parent.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ChildComponent } from './components/test/child/child.component';
 import { GrandparentComponent } from './components/test/grandparent/grandparent.component';
-import { ChildMessageService } from './services/child-message.service';
+import { ParentComponent } from './components/test/parent/parent.component';
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'contact',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    // redirectTo: 'contact',
+  },
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +45,14 @@ import { ChildMessageService } from './services/child-message.service';
     ParentComponent,
     ChildComponent,
     GrandparentComponent,
+    HomeComponent,
+    ContactComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { enableTracing: false }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [HeaderComponent, FooterComponent, MainContentComponent],
