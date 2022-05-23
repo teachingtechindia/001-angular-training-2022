d70d40074baedbbfd89a8d9438a1493e7678462f
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-05-23 16:50:02 +0530

    Create a simple observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    // const p = new Promise((resolve, reject) => {
+    //   console.log('Promise started');
+    //   resolve('Promise resolved');
+    // });
+
+    // p.then((data) => {
+    //   console.log('Promise resolved with data: ', data);
+    // });
+
+    // Creating Observable
+    const myObservable = new Observable((subscriberOrObserverOrConsumer) => {
+      subscriberOrObserverOrConsumer.next(1);
+      subscriberOrObserverOrConsumer.next(2);
+
+      setInterval(() => {
+        subscriberOrObserverOrConsumer.next(new Date());
+        console.log('inside onbservable');
+      }, 1000);
+    });
+
+    // Subscribing to Observable
+    const subscription = myObservable.subscribe((data) => {
+      console.log(data);
+    });
+
+    // Unsubscribing from Observable
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
+  }
+}
