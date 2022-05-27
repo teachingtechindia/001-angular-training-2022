77cac668e1de364a950cfc33a8b6440c37719714
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-05-27 17:30:13 +0530

    ngFor directive in angular

--- src/app/pages/movies/list/list.component.ts
+++ src/app/pages/movies/list/list.component.ts
@@ -7,12 +7,15 @@ import { MoviesRestService } from '../services/movies-rest.service';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  movies: any = [];
+
   constructor(private moviesRestService: MoviesRestService) {}
 
   ngOnInit(): void {
     this.moviesRestService.getMovieList().subscribe(
       (data) => {
         console.log('VK: MovieList: data', data);
+        this.movies = data;
       },
       (error) => {
         console.log('VK: MovieList: error', error);
