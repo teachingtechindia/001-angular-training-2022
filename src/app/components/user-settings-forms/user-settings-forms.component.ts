e6b41c1e998a354bae73a7ca49d42af73e470619
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-06-03 17:33:20 +0530

    Form: 2 binded data binding

--- src/app/components/user-settings-forms/user-settings-forms.component.ts
+++ src/app/components/user-settings-forms/user-settings-forms.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface UserSettings {
+  name: string;
+  emailOffers: boolean;
+  interfaceStyle: string;
+  subscriptionType: string;
+  notes: string;
+}
+
 @Component({
   selector: 'at-user-settings-forms',
   templateUrl: './user-settings-forms.component.html',
   styleUrls: ['./user-settings-forms.component.scss'],
 })
 export class UserSettingsFormsComponent implements OnInit {
+  userSettings: UserSettings = {
+    name: 'Vivek Kumar',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'I like Angular framework',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
