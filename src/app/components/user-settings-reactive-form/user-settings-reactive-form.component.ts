dcd56b62183a8afb4442d1b4716febc1870b465b
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-06-07 17:01:31 +0530

    using form builder instaed of forControl and Formgroup constructor

--- src/app/components/user-settings-reactive-form/user-settings-reactive-form.component.ts
+++ src/app/components/user-settings-reactive-form/user-settings-reactive-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'at-user-settings-reactive-form',
@@ -10,20 +15,31 @@ export class UserSettingsReactiveFormComponent implements OnInit {
   public userSettingForm!: FormGroup;
   public formSubmitted = false;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.createForm();
   }
 
   createForm() {
-    this.userSettingForm = new FormGroup({
-      name: new FormControl('Angular training', {
-        validators: Validators.required,
-        updateOn: 'change',
-      }),
-      emailOffers: new FormControl(true),
-      interfaceStyle: new FormControl(''),
-      subscriptionType: new FormControl(''),
-      notes: new FormControl(''),
+    // this.userSettingForm = new FormGroup({
+    //   name: new FormControl('Angular training', {
+    //     validators: Validators.required,
+    //     updateOn: 'change',
+    //   }),
+    //   emailOffers: new FormControl(true),
+    //   interfaceStyle: new FormControl(''),
+    //   subscriptionType: new FormControl(''),
+    //   notes: new FormControl(''),
+    // });
+
+    this.userSettingForm = this.fb.group({
+      name: [
+        'Vivek Kumar',
+        { validators: Validators.required, updateOn: 'change' },
+      ],
+      emailOffers: [''],
+      interfaceStyle: [''],
+      subscriptionType: [''],
+      notes: [''],
     });
   }
 
