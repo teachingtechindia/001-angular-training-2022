21d49b80875f627010ecc8b5c023bd71fcbab984
techingtechindia@gmail.com <vvakekumar@gmail.com>
2022-06-10 17:23:29 +0530

    Observable unicase example

--- src/app/services/ping-pong.service.ts
+++ src/app/services/ping-pong.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { interval, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +7,21 @@ import { Subject } from 'rxjs';
 export class PingPongService {
   msgSubject = new Subject<string>();
 
-  constructor() {}
+  constructor() {
+    // let i = 1;
+    // const o1$ = Observable.create((observer: any) => {
+    //   setInterval(() => {
+    //     observer.next(new Date());
+    //     i++;
+    //   }, 2000 + i * 100);
+    // });
+    // o1$.subscribe((x: any) => {
+    //   console.log('o1$ -- 1 --', x);
+    // });
+    // setTimeout(() => {
+    //   o1$.subscribe((x: any) => {
+    //     console.log('o1$ -- 2 --', x);
+    //   });
+    // }, 3000);
+  }
 }
